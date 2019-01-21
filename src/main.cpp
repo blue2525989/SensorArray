6354c443d32689b2ec8f7b14383dde308d765835
blue2525989 <blue2525989@gmail.com>
2019-01-21 11:16:36 -0600

    commited to sensors and setup, working out logic

--- src/main.cpp
+++ src/main.cpp
@@ -1,92 +1,114 @@
 #include <Arduino.h>
 #include <SD.h>
 #include <SPI.h>
-// #include <dht.h>
 
-#define FIRE_MIN = 0
-#define FIRE_MAX = 1024
+#define FIRE_MIN 0
+#define FIRE_MAX 1024
+#define FIRE_PIN A0
+#define TILT_PIN A1
+#define SD_PIN 7
+#define TEMP_PIN A2
+// using a delay in main loop of 100ms and wanting to sample temp every 5sec so
+// every loop iteration is LOG_TEMP_TIME/counter+1 = (delay+100 * 50) = 5000
+#define LOG_TEMP_TIME 50
+
 File sensorData;
-// DHT dht;
+
+
+void readSensorsAndLogLoop() {
+    // for counting when to sample
+    uint8_t counter = 0;
+
+
+
+    while(true) {
+        if (analogRead(TILT_PIN) == 0) {
+            sensorData.print("{tilt: TRUE");
+            sensorData.println("}");
+        }
+
+        float firePinVal = analogRead(FIRE_PIN);
+        if (firePinVal < 100) {
+            sensorData.print("{fire: {range: ");
+            sensorData.print(firePinVal);
+            sensorData.println(", active: TRUE} }");
+        }
+
+        if (counter == LOG_TEMP_TIME) {
+            float val = analogRead(TEMP_PIN);
+            float cel = (5.0 * val * 100.0)/1024.0;
+            float f = ((cel*9)/5) + 32;
+
+            sensorData.print("{tempF: ");
+            sensorData.print(f);
+            sensorData.println("}");
+        }
+        // delay 100ms and increament counter
+        delay(100);
+        counter++;
+
+
+        // close the file
+        sensorData.close();
+    }
+}
+
+
+
+void readFile() {
+    if (sensorData) {
+        Serial.println("{data:");
+        while (sensorData.available()) {
+            Serial.write(sensorData.read());
+        }
+        Serial.println("\n}");
+    } else {
+        // if the file didn't open, print an error:
+        Serial.println("error opening data.txt");
+    }
+    Serial.flush();
+}
+
+void openFile() {
+    // open file
+    while (!sensorData) {
+        sensorData = SD.open("data.txt", FILE_WRITE);
+    }
+    Serial.println("SD initialized, data.txt file opened for logging.");
+}
+
+void closeFile() {
+    sensorData.close();
+}
 
 int main() {
     init();
-    // dht dht11;
-
-
+    // init serial port
     Serial.begin(9600);
     while (!Serial) {
     }
 
-    uint8_t counter = 0;
-
-    // pinMode(8, INPUT);
-    //
-    // dht.setup(7);
-
+    // temp analog
+    pinMode(TEMP_PIN, INPUT);
     // fire sensor
-    pinMode(A0, INPUT);
-    // mercury switch
-    pinMode(A1, INPUT);
+    pinMode(FIRE_PIN, INPUT);
+    // reed switch
+    pinMode(TILT_PIN, INPUT);
+    // init SD
+    while (!SD.begin(SD_PIN)) {
+    }
+
+    // open file
+    openFile();
+
     while(true) {
-        Serial.println(analogRead(A0));
-        Serial.println(analogRead(A1));
-        // works on uno but not nano
-        // if (dht.getStatus() == 0) {
-        //     Serial.println("ahaha 0 status");
-        // }
-        // if (dht.getStatusString() == "OK") {
-        //     Serial.println("ahahahha OK status");
-        // }
-        // Serial.println(dht.getStatus());
-        // float farh = dht.toFahrenheit(dht.getTemperature());
-        // float hum = dht.getHumidity();
-        // Serial.println(isnan(farh));
-        //
-        // Serial.println(farh);
-        // Serial.println(hum);
-
-
-      //   Serial.println("high");
-      //   digitalWrite(8, HIGH);
-      //
-      // delay(1000);
-      //     Serial.println("low");
-      // digitalWrite(8, LOW);
-      // delay(dht.getMinimumSamplingPeriod() * 2);
-
-      delay(1000);
-        counter++;
-        if (counter == 60) {
-            break;
+        if (Serial.read() == "Stop") {
+            Serial.println("Reading file.");
+            readFile();
+        } else {
+            Serial.println("logging data.");
+            readSensorsAndLogLoop();
         }
-    }
-
-    while (!SD.begin(7)) {
-    }
-    sensorData = SD.open("data.txt", FILE_WRITE);
-
-
-
-    // if the file opened okay, write to it:
-    if (sensorData) {
-        // close the file
-        sensorData.close();
-      } else {
-          // if the file didn't open, print an error:
-          Serial.println("error opening sensorData.txt");
-    }
-
-    // re-open the file for reading:
-    sensorData = SD.open("data.txt");
-    if (sensorData) {
-        Serial.println("data.txt:");
-        while (sensorData.available()) {
-            Serial.write(sensorData.read());
-        }
-        sensorData.close();
-    } else {
-        // if the file didn't open, print an error:
-        Serial.println("error opening sensorData.txt");
     }
     Serial.flush();
 }
